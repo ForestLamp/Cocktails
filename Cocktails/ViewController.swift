7655c670f0ac351a4027e0c3364d8ec456e5752a
ForestLamp <y100b@yandex.ru>
2022-03-28 16:36:59 +0300

    Initial Commit

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Alex Ch. on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
